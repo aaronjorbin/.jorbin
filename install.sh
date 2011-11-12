05abbf5eb1a968e11ee6caccc3cc2b5d84ff3e4f
Aaron Jorbin <aaron@jorb.in>
2011-11-12 00:05:18 -0500

    add submodule vim

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+$CURRENTDIR = `pwd`
 
 # .bashrc
 ln -sfnv ~/.jorbin/bashrc ~/.bashrc
 source ~/.bashrc
 
 # ssh.conf
-ln -sfnv ~/.jorbin/ssh/config ~/.ssh/config
+if [ -f ~/.jorbin/ssh/config ]
+    then
+    ln -sfnv ~/.jorbin/ssh/config ~/.ssh/config
+fi
+
+# vim
+if [ -d ~/.jorbin/.vim ]
+    then
+    ln -sfnv ~/.jorbin/.vim ~/.vim
+    ln -sfnv ~/.jorbin/.vim/vimrc ~/.vimrc
+    cd ~/.vim
+    git submodule init
+    git submodule update
+    cd $CURRENTDIR
+fi
